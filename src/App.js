de3bd6f339c508d4b1a21af6ec03e40f816314f7
Gonai <margaret.szymanik@gmail.com>
2026-04-19 14:27:48 +0200

    creating web structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Projekt Gosi
-        </p>
+        <Header/>
+        <Nav/>
+        <Main/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+function Main() {
+  return (
+    <main>
+      <section>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+        </p>
+        <button>Reserve a Table</button>
+      </section>
+
+      <section>
+        <h2>This week's specials!</h2>
+
+        <article>
+          <h3>Greek Salad</h3>
+          <p>$12.99</p>
+          <p>Description of the dish...</p>
+        </article>
+
+        <article>
+          <h3>Bruschetta</h3>
+          <p>$5.99</p>
+          <p>Description of the dish...</p>
+        </article>
+
+        <article>
+          <h3>Lemon Dessert</h3>
+          <p>$5.00</p>
+          <p>Description of the dish...</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Main;

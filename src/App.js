c36687778ce71b2a839156766ef6ed3fcbfcf7f9
Gonai <margaret.szymanik@gmail.com>
2026-04-20 15:36:41 +0200

    creating routes and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Order from "./components/Order";
+import Reservations from "./components/Reservations";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <Main/>
-        <Footer/>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
